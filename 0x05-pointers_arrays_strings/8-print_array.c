481cce395a6fb4686076b0653824ad1abf4adf04
mimya <mimya.mohammed@gmail.com>
2023-09-18 14:02:57 +0100

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array -  prints arrays
+ *
+ * @a: arg a
+ * @n: num
+ *
+ * Return: string to stdout
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+		printf("\n");
+}
