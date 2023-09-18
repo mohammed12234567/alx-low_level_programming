012d03b025e64bd685e63ae277def746ed0d8266
mimya <mimya.mohammed@gmail.com>
2023-09-18 13:36:38 +0100

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: pointer to string
+ *
+ * Return: str to standout
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
